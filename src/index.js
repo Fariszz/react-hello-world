ee7b8a913b6458a7b09d3de311e6b4c466dc0083
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2021-05-07 13:50:06 +0700

    component and container

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './component/HelloComponent'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
