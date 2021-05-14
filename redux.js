26d19f0bcd324ebda68c2d218499a2b5d1cef0ad
Muhammad Faris Hadi Mulyo <ziraflyo@gmail.com>
2021-05-14 23:27:26 +0700

    Update redux.js

--- redux.js
+++ redux.js
@@ -7,15 +7,33 @@ const initialState = {
 }
 
 // Reducer
-const rootReducer = (state = initialState, action) =>{
-    return state;
+const rootReducer = (state = initialState, action) =>{    
+    switch(action.type){
+        case 'ADD_AGE':
+            return {
+                ...state,
+                age: state.age + 1
+            }
+        case 'CHANGE_VALUE':
+            return{
+                ...state,
+                value: state.value + action.newValue
+            }
+        default:
+            return state;
+    }
 }
 
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState())
 
+// Subscription
+store.subscribe(() => {
+    console.log('store change: ', store.getState())
+})
 
 // Dispatching Action
-
-// Subscription
+store.dispatch({type: 'ADD_AGE'})
+store.dispatch({type: 'CHANGE_VALUE', newValue: 12})
+console.log(store.getState())
